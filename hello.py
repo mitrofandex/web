7af450787e19aac76e585e18b7e3f00112195dbb
vyacheslav <46143172+mitrofandex@users.noreply.github.com>
2019-01-16 21:11:29 +0000

    second task done

--- hello.py
+++ hello.py
@@ -1,3 +1,3 @@
 def app(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    return env['QUERY_STRING'].replace('&', '\n')
+    return [bytes(x + '\n', 'ascii') for x in env['QUERY_STRING'].split('&')]
