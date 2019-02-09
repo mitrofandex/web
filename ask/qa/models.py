f190cf47e8953c49f8658257386b9be050e91b1d
vyacheslav <46143172+mitrofandex@users.noreply.github.com>
2019-02-09 18:05:14 +0300

    asda

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -17,7 +17,7 @@ class Question(models.Model):
     added_at = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    likes = models.ManyToManyField(User, related_name='user_likes')
+    likes = models.ManyToManyField(User, related_name='likes_set')
 
 
 class Answer(models.Model):
